87bc839a970c3a35af4ee6615b2c76378cae63dc
Tess <tessneau@gmail.com>
2019-05-06 15:01:04 -0400

    done

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require  'pry'
+
+def dictionary
+substitutes = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" =>"2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter(tweet)
+  short_array = tweet.split.map { |word| dictionary.keys.include?(word.downcase) ? dictionary[word.downcase] : word }
+  short_array.join(' ')
+end
+
+def bulk_tweet_shortener(tweet_array)
+  tweet_array.each { |tweet| puts word_substituter(tweet) }
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.length > 140 ? tweet[0..139] : tweet
+end
